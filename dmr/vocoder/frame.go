ac1e7e9ec8cc935e571061e19051da7f41be3f84
Jacob McSwain <jacob.a.mcswain@gmail.com>
2023-10-19 12:14:42 -0500

    vocoder: allow encoding with FEC

--- dmr/vocoder/frame.go
+++ dmr/vocoder/frame.go
@@ -21,6 +21,78 @@ func (vf *VocoderFrame) ToString() string {
 	return fmt.Sprintf("{ DecodedFrame: %014s }", hex.EncodeToString(data[:]))
 }
 
+// Encode takes the decoded bits and encodes them into a 216 bit frame
+func (vf *VocoderFrame) Encode() [72]byte {
+	var ambe72 [72]byte
+
+	var ambe49 = vf.DecodedBits
+
+	var aOrig uint32 = 0
+	var bOrig uint32 = 0
+	var cOrig uint32 = 0
+	var MASK uint32 = 0x000800
+	ambe49[9] = 0
+
+	for i := 0; i < 12; i, MASK = i+1, MASK>>1 {
+		n1 := uint32(i + 0)
+		n2 := uint32(i + 12)
+		if ambe49[n1] == 1 {
+			aOrig |= MASK
+		}
+		if ambe49[n2] == 1 {
+			bOrig |= MASK
+		}
+	}
+
+	MASK = 0x1000000
+	for i := 0; i < 25; i, MASK = i+1, MASK>>1 {
+		n := uint32(i + 24)
+		if ambe49[n] == 1 {
+			cOrig |= MASK
+		}
+	}
+
+	a := fec.Golay_24_12_8_EncodingTable[aOrig]
+
+	// The PRNG
+	p := fec.PRNG_TABLE[aOrig] >> 1
+
+	b := fec.Golay_23_12_7_EncodingTable[bOrig] >> 1
+	b ^= p
+
+	MASK = 0x800000
+	for i := 0; i < 24; i, MASK = i+1, MASK>>1 {
+		aPos := aTable[i]
+		if (a & MASK) != 0 {
+			ambe72[aPos] = 1
+		} else {
+			ambe72[aPos] = 0
+		}
+	}
+
+	MASK = 0x400000
+	for i := 0; i < 23; i, MASK = i+1, MASK>>1 {
+		bPos := bTable[i]
+		if (b & MASK) != 0 {
+			ambe72[bPos] = 1
+		} else {
+			ambe72[bPos] = 0
+		}
+	}
+
+	MASK = 0x1000000
+	for i := 0; i < 25; i, MASK = i+1, MASK>>1 {
+		cPos := cTable[i]
+		if (cOrig & MASK) != 0 {
+			ambe72[cPos] = 1
+		} else {
+			ambe72[cPos] = 0
+		}
+	}
+
+	return ambe72
+}
+
 var aTable = []int{0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44,
 	48, 52, 56, 60, 64, 68, 1, 5, 9, 13, 17, 21}
 
